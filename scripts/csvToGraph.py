ad582032415310a3c6461fdb3795251ca1b2eb01
Samuel Crawford <sam.crawford82@gmail.com>
2024-07-10 15:27:26 -0400

    Modify graph code to separate static approaches to their own graph

--- scripts/csvToGraph.py
+++ scripts/csvToGraph.py
@@ -28,27 +28,48 @@ names = [removeInParens(n) for n in names if type(n) is str]
 parents = [removeInParens(p).strip(",").split(", ")
            if type(p) is str else [] for p in parents]
 
-dot = [
-    "\\documentclass{article}",
-    "\\usepackage{graphicx}",
-    "\\usepackage[pdf]{graphviz}",
-    "\\usepackage{tikz}",
-    "\\usetikzlibrary{arrows,shapes}",
-    "",
-    "\\begin{document}",
-    "\\digraph{approachGraph}{",
-    "rankdir=BT;",
-    "",
-]
+dot, staticDot = [], []
+staticApproaches = {
+    'ConcreteExecution', 'SymbolicExecution', 'InductiveAssertionMethods',
+    'ContentChecking', 'ModelVerification'}
+staticKeywords = {'Audit', 'Inspection' 'Proof', 'Review', 'Static', 'Walkthrough'}
+dynamicExceptions = {}
 
-for name in names:
+def addNode(name, staticOnly = False):
     dashed = False
     if "?" in name:
         name = name.replace("?", "")
-        dashed = True
-    dot.append(f"{formatApproach(name)} [label={lineBreak(name.strip(")"))}{",style=dashed" if dashed else ""}];")
+        dashed = not staticOnly
+    extra = [
+        f"label={lineBreak(name.strip(")"))}",
+        f'style="{",".join([
+            s for s in ["dashed" if dashed else "",
+                        "filled" if staticOnly else ""] if s
+            ])}"' if dashed or staticOnly else ""
+    ]
+    nameLine = f"{formatApproach(name)} [{",".join([e for e in extra if e])}];"
+
+    for k in staticKeywords:
+        if k in name and name not in dynamicExceptions:
+            staticApproaches.add(formatApproach(name))
+            break
+    
+    if staticOnly:
+        staticDot.append(nameLine)
+        return
+
+    if formatApproach(name) in staticApproaches:
+        staticDot.append(nameLine)
+    else:
+        dot.append(nameLine)
+
+for name in names:
+    addNode(name)
 
 dot.append("")
+staticDot.append("")
+
+workingStaticSet = staticApproaches.copy()
 
 for name, parent in zip(names, parents):
     # if [x for x in parent + [name] if "keyword" in x.lower()]:
@@ -58,12 +79,42 @@ for name, parent in zip(names, parents):
         dashed = False
         if "?" in p:
             dashed = True
-        dot.append(f"{formatApproach(name).replace(" ", "")} -> {formatApproach(p)}{"[style=dashed]" if dashed else ""};")
+        fname, fp = formatApproach(name), formatApproach(p)
+        parentLine = f"{fname} -> {fp}{"[style=dashed]" if dashed else ""};"
+        if fname in staticApproaches or fp in staticApproaches:
+            if fname not in workingStaticSet:
+                print("name", name)
+                addNode(name, staticOnly=True)
+                workingStaticSet.add(fname)
+            elif fp not in workingStaticSet:
+                print("p", p)
+                addNode(p, staticOnly=True)
+                workingStaticSet.add(fp)
+            staticDot.append(parentLine)
+        else:
+            dot.append(parentLine)
 
-dot += [
-    "}",
-    "\\end{document}",
-]
+def make_dot_file(lines, filename):
+    lines = [
+        "\\documentclass{article}",
+        "\\usepackage{graphicx}",
+        "\\usepackage[pdf]{graphviz}",
+        "\\usepackage{tikz}",
+        "\\usetikzlibrary{arrows,shapes}",
+        "",
+        "\\begin{document}",
+        f"\\digraph{{{filename}}}{{",
+        "rankdir=BT;",
+        "",
+    ] + lines + [
+        "}",
+        "\\end{document}",
+    ]
 
-with open("assets/graphs/approachGraph.tex", "w") as outFile:
-    outFile.writelines(line + '\n' for line in dot)
+    with open(f"assets/graphs/{filename}.tex", "w") as outFile:
+        outFile.writelines(line + '\n' for line in lines)
+
+make_dot_file(dot, "approachGraph")
+make_dot_file(staticDot, "staticGraph")
+
+print(staticApproaches)
