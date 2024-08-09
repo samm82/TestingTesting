dc17213f6befaa962592d6e95f2baefe4051608d
Samuel Crawford <sam.crawford82@gmail.com>
2024-08-08 21:06:26 -0400

    Format generic Structural Analysis notes from Patton

--- scripts/csvToGraph.py
+++ scripts/csvToGraph.py
@@ -115,8 +115,8 @@ categoryDict = {
     "Type": ([], []),
 }
 
-UNSURE_KEYWORDS = ["implied", "inferred", "can be", "usually", "most", "often",
-                   "if", "although"]
+UNSURE_KEYWORDS = ["implied", "inferred", "can be", "ideally", "usually", "most",
+                   "often", "if", "although"]
 def isUnsure(name):
     return any(unsure in name for unsure in
                {"?", " (Testing)"}.union(f"({term}" for term in UNSURE_KEYWORDS))
